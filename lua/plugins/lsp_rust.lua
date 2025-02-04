b108f338b2cdfc0a7aae359c105b91860d02ed36
Ryan Odeneal <ryan@ilj.com>
2025-02-04 08:42:30 -0600

    Removing grey inference in code and adding indent option to tree sitter.

--- lua/plugins/lsp_rust.lua
+++ lua/plugins/lsp_rust.lua
@@ -8,6 +8,13 @@ return {
         },
         rust_analyzer = { enabled = false },
       },
+
+      -- This will give you the grey type inference in rust. I am not a fan as 
+      -- I want all of my code to be inside 80 character columns when I am 
+      -- reading and interacting with it.
+      inlay_hints = {
+        enabled = false,
+      },
     },
   },
 }
